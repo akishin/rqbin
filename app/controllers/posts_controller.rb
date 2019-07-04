6f9dea43b98bdc481d5e74e5324ea9dd377698c3
akishin <akishin999@gmail.com>
2019-07-04 19:48:08 +0900

    fix Encoding::UndefinedConversionError
    Encoding::UndefinedConversionError ("\xE3" from ASCII-8BIT to UTF-8)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,10 @@ class PostsController < ApplicationController
     post.params = params.to_json
     h = {}
     request.env.each do |k, v|
-      h[k] = v if v.class == String
+      if v.class == String
+        v.force_encoding(Encoding::UTF_8) if v.encoding == Encoding::ASCII_8BIT
+        h[k] = v
+      end
     end
     post.headers = h.to_json
     post.http_method = request.request_method
